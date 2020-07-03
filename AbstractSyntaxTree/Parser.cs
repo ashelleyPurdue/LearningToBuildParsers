b85b478b120a201b928ef3abae5dcd961a4a2ce2
Alex Shelley <alexthepeppershelley@gmail.com>
2020-07-03 16:15:56 -0400

    Add stub for Parser

---
+++ AbstractSyntaxTree/Parser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AbstractSyntaxTree
+{
+  public class Parser
+  {
+    public AstRoot Parse(IEnumerable<char> sourceCode)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
