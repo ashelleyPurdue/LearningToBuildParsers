9a62902cc1f2ea697abd8faf5eeed1586190c332
Alex Shelley <alexthepeppershelley@gmail.com>
2020-07-06 16:38:05 -0400

    Extract helper function AssertEqualIgnoringOrder

---
+++ AbstractSyntaxTree.UnitTests/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AbstractSyntaxTree.UnitTests
+{
+  public static class Utils
+  {
+    public static void AssertEqualIgnoringOrder<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+    {
+      Assert.Equal(expected.Count(), actual.Count());
+
+      foreach (T item in expected)
+        Assert.Contains(item, actual);
+    }
+  }
+}
