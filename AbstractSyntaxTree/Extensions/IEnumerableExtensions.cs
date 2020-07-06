18b1bf5e27fd226830c3b8be24acfb80436b0be8
Alex Shelley <alexthepeppershelley@gmail.com>
2020-07-06 15:04:49 -0400

    Refactor Parser Lexer and TokenWalker

--- AbstractSyntaxTree/Extensions/IEnumerableExtensions.cs
+++ AbstractSyntaxTree/Extensions/IEnumerableExtensions.cs
@@ -6,51 +6,7 @@ namespace AbstractSyntaxTree
 {
   public static class IEnumerableExtensions
   {
-    /// <summary>
-    /// Like TakeWhile, except it puts the remaining items
-    /// in an out parameter.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="sequence"></param>
-    /// <param name="stuffThatWasChopped"></param>
-    /// <param name="predicate"></param>
-    /// <returns></returns>
-    public static IEnumerable<T> ChopWhile<T>(this IEnumerable<T> sequence, out IEnumerable<T> rest, Func<T, bool> predicate)
-    {
-      rest = sequence.SkipWhile(predicate);
-      return sequence.TakeWhile(predicate);
-    }
-
     public static string AsString(this IEnumerable<char> charSeq)
       => new string(charSeq.ToArray());
-
-    public static IEnumerable<char> ChopWord(this IEnumerable<char> str, out IEnumerable<char> rest)
-      => str.ChopWhile(out rest, c => char.IsLetterOrDigit(c));
-
-    public static IEnumerable<char> TakeWord(this IEnumerable<char> str)
-      => str.TakeWhile(c => char.IsLetterOrDigit(c));
-
-    /// <summary>
-    /// Expects the next characters in the sequence to match a string
-    /// Throws an exception if they do not.
-    /// Returns the rest of the sequence after the expected string
-    /// </summary>
-    /// <param name="src"></param>
-    /// <param name="expectedSequence"></param>
-    /// <returns></returns>
-    public static IEnumerable<char> ExpectNext(this IEnumerable<char> src, string expectedSequence)
-    {
-      var actualSequence = src
-        .Take(expectedSequence.Length)
-        .AsString();
-
-      if (actualSequence != expectedSequence)
-        throw new Exception($@"Expected the next bit of code to be ""{expectedSequence}"", but it was ""{actualSequence}""");
-
-      return src.Skip(expectedSequence.Length);
-    }
-
-    public static IEnumerable<char> SkipWhitespace(this IEnumerable<char> str)
-      => str.SkipWhile(c => char.IsWhiteSpace(c));
   }
 }
--- AbstractSyntaxTree/Parser.cs
+++ AbstractSyntaxTree/Parser.cs
@@ -6,46 +6,54 @@ namespace AbstractSyntaxTree
 {
   public class Parser
   {
+    private readonly ISet<string> _keywords = new HashSet<string>
+    {
+      "class"
+    };
+
     public AstRoot Parse(IEnumerable<char> src)
     {
+      var lexer = new Lexer(_keywords);
+      var tokens = new TokenWalker(lexer.ToTokens(src));
+
       var root = new AstRoot();
 
       // Parse all the classes
       root.Classes = new List<ClassDefinition>();
 
-      while (src.SkipWhitespace().Any())
+      while (!tokens.IsEmpty())
       {
-        root.Classes.Add(ParseClass(src, out src));
+        switch (tokens.Peek())
+        {
+          case KeywordToken k when k.Content == "class":
+            root.Classes.Add(ParseClass(tokens));
+            break;
+
+          default: throw new Exception("Unexpected token");
+        }
       }
       
       return root;
     }
 
-    private ClassDefinition ParseClass(IEnumerable<char> src, out IEnumerable<char> rest)
+    private ClassDefinition ParseClass(TokenWalker tokens)
     {
       // Look for the class keyword
-      src = src
-        .SkipWhitespace()
-        .ExpectNext("class")
-        .SkipWhitespace();
+      tokens.ConsumeKeyword("class");
 
       // Grab the name
       var classDef = new ClassDefinition();
-      classDef.Name = src
-        .SkipWhitespace()
-        .ChopWord(out src)
-        .AsString();
+      classDef.Name = tokens
+        .Consume<WordToken>()
+        .Content;
 
-      // TODO: Parse the functions.  For now, just enforce them to be empty.
-      src = src
-        .SkipWhitespace()
-        .ExpectNext("{")
-        .SkipWhitespace()
-        .ExpectNext("}");
+      // TODO: Parse the functions.  For now, just enforce that
+      // it's an empty class
+      tokens.Consume<OpenCurlyToken>();
+      tokens.Consume<CloseCurlyToken>();
 
       classDef.Functions = new List<FunctionDefinition>();
 
-      rest = src;
       return classDef;
     }
   }
