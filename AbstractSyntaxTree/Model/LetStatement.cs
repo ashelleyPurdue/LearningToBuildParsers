7bae0fd1e2181ceb3aa5c040277950b338529493
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-02 14:05:28 -0400

    Add LetStatement

---
+++ AbstractSyntaxTree/Model/LetStatement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree
+{
+  public class LetStatement : IStatement
+  {
+    public string Name { get; set; }
+    // TODO: Type
+    // TODO: Initial value expression
+  }
+}
