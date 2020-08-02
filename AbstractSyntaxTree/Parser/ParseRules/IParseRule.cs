005828d65d5f962d6fd226f7b541bf3084be6a9f
Alex Shelley <ashelley@purdue.edu>
2020-08-02 17:30:02 -0400

    Add IParseRule

---
+++ AbstractSyntaxTree/Parser/ParseRules/IParseRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree
+{
+  public interface IParseRule<TNode>
+  {
+    bool IsStartOfNode(TokenWalker walker);
+    (TNode node, TokenWalker rest) ParseNode(TokenWalker walker);
+  }
+}
