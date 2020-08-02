41f38f3bf1492cd984fa3e22f001b36a9a88400f
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-02 14:43:30 -0400

    Split RootParseTests into separate files

---
+++ AbstractSyntaxTree.UnitTests/ClassParseTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace AbstractSyntaxTree.UnitTests
+{
+  public class ClassParseTests
+  {
+    [Fact]
+    public void It_Can_Parse_Multiple_Functions_In_A_Class()
+    {
+      const string src =
+      @"
+        class ThingDoer
+        {
+          function DoThing() { }
+          function DoOtherThing()
+          {
+          }
+        }
+      ";
+
+      string[] expectedFuncNames = new[]
+      {
+        "DoThing",
+        "DoOtherThing"
+      };
+
+      var classDef = ParseSingleClass(src);
+
+      // Assert that all of the function names are present, and no others.
+      var actualFuncNames = classDef
+        .Functions
+        .Select(f => f.Name);
+      Utils.AssertEqualIgnoringOrder(expectedFuncNames, actualFuncNames);
+    }
+
+    /// <summary>
+    /// Accepts a source code fragment in the form of
+    /// "class ClassName { [members] }"
+    /// </summary>
+    /// <param name="src"></param>
+    /// <returns></returns>
+    private ClassDefinition ParseSingleClass(string src)
+    {
+      var p = new Parser();
+      var root = p.Parse(src);
+
+      Assert.Single(root.Classes);
+      return root.Classes[0];
+    }
+  }
+}
---
+++ AbstractSyntaxTree.UnitTests/FunctionParseTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AbstractSyntaxTree.UnitTests
+{
+  public class FunctionParseTests
+  {
+
+    [Fact]
+    public void It_Can_Parse_An_Empty_Function()
+    {
+      var func = ParseSingleFunction("function DoThing() { }");
+      Assert.Equal("DoThing", func.Name);
+      Assert.Empty(func.Statements);
+    }
+
+    [Fact]
+    public void It_Can_Parse_Simple_Variable_Decls_In_A_Function()
+    {
+      const string src =
+      @"
+        function DoThing() 
+        {
+          let fooVar;
+          let barVar;
+        }
+      ";
+
+      string[] expectedVarNames = new[]
+      {
+        "fooVar",
+        "barVar"
+      };
+
+      var func = ParseSingleFunction(src);
+
+      // The order of the statements IS important this time around.
+      for (int i = 0; i < expectedVarNames.Length; i++)
+      {
+        var statement = func.Statements[i];
+        var expectedName = expectedVarNames[i];
+
+        Assert.IsAssignableFrom<LetStatement>(statement);
+        var letStatement = (LetStatement)statement;
+
+        Assert.Equal(expectedName, letStatement.Name);
+      }
+    }
+
+    /// <summary>
+    /// Accepts a source code fragement representing a function:
+    /// "function FuncName([params here]) { [statements here] }".
+    /// Returns the function node that would be parsed from that.
+    /// </summary>
+    /// <param name="src"></param>
+    /// <returns></returns>
+    private FunctionDefinition ParseSingleFunction(string src)
+    {
+      var p = new Parser();
+      string biggerSrc =
+      $@"
+        class ThingDoer
+        {{
+          {src}
+        }}
+      ";
+
+      AstRoot root = p.Parse(biggerSrc);
+      Assert.Single(root.Classes);
+
+      var funcs = root.Classes[0].Functions;
+      Assert.Single(funcs);
+
+      return funcs[0];
+    }
+  }
+}
