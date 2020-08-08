64f68c287bef0652b6ce1b40446a59d141b76ed5
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-08 16:23:57 -0400

    Add RuleSet

---
+++ AbstractSyntaxTree/Parser/Rules/RuleSet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AbstractSyntaxTree
+{
+  public class RuleSet
+  {
+    private readonly List<IParseRule> _rules = new List<IParseRule>();
+
+    public RuleSet AddRule(IParseRule rule)
+    {
+      _rules.Add(rule);
+      return this;
+    }
+
+    public IEnumerable<NextTokenResult> TryParse(IEnumerable<Token> tokens)
+    {
+      // We're going to start with a set of candidate rules that could
+      // potentially be matched by the upcoming tokens.
+      // Each token will be fed to every candidate rule.
+      // The first candidate rule to match will be the "winner", and we'll
+      // yield its node.
+      // If a candidate rule fails, it is eliminated from the list of candidates.
+      // If all of the candidates are eliminated, then this ruleset is fails.
+
+      List<IEnumerator<NextTokenResult>> candidateRules = _rules
+        .Select(r => r.TryParse(tokens).GetEnumerator())
+        .ToList();
+
+      foreach (var token in tokens)
+      {
+        var rules = candidateRules;
+        candidateRules = new List<IEnumerator<NextTokenResult>>();
+
+        // Feed this token to every rule.
+        foreach (var rule in rules)
+        {
+          if(!rule.MoveNext())
+            continue;
+
+          NextTokenResult result = rule.Current;
+
+          // If this rule matched, then it wins.
+          // The ruleset is complete, so yield its result and stop.
+          if (result.state == RuleMatchState.Complete)
+          {
+            yield return result;
+            yield break;
+          }
+
+          // If this rule failed, then it is eliminated.
+          // We eliminate it by not adding it back into the
+          // candidate rules.
+          if (result.state == RuleMatchState.Fail)
+            continue;
+
+          // This rule is good so far, so don't eliminate it.
+          candidateRules.Add(rule);
+        }
+
+        // If there is at least one remaining candidate, then we're good so far.
+        if (candidateRules.Count > 0)
+          yield return NextTokenResult.GoodSoFar(null);
+      }
+    }
+  }
+}
