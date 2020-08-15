790c06216b317894a8aa6d2bf464db86fc3ebeda
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-15 15:51:52 -0400

    Implement let statement parsing.

--- AbstractSyntaxTree/Parser/Rules/FunctionParseRule.cs
+++ AbstractSyntaxTree/Parser/Rules/FunctionParseRule.cs
@@ -21,7 +21,48 @@ namespace AbstractSyntaxTree
       yield return ExpectSymbol(")", node);
       yield return ExpectSymbol("{", node);
 
+      // Keep parsing let statements until we encounter a closing bracket.
+      // TODO: Replace this with a RuleParser
+      while (_currentToken.Content != "}")
+      {
+        if (_currentToken.Content == "let")
+        {
+          // Parse the entirety of this let-statement, converting its
+          // NextTokenResults into ones for a FunctionDefinition.
+          foreach (var result in ParseLetStatement())
+          {
+            switch (result.state)
+            {
+              case RuleMatchState.GoodSoFar:
+                yield return NextTokenResult.GoodSoFar(node);
+                break;
+              case RuleMatchState.Fail:
+                yield return result;
+                break;
+              case RuleMatchState.Complete:
+                node.Statements.Add((LetStatement)result.node);
+                yield return NextTokenResult.GoodSoFar(node);
+                break;
+            }
+          }
+        }
+      }
       var end = ExpectSymbol("}", node);
+      end.state = RuleMatchState.Complete;
+      yield return end;
+    }
+
+    private IEnumerable<NextTokenResult> ParseLetStatement()
+    {
+      var node = new LetStatement();
+
+      yield return ExpectKeyword("let", node);
+
+      string varName;
+      yield return ExtractToken(TokenType.Word, node, out varName);
+      node.Name = varName;
+
+      var end = ExpectSymbol(";", node);
       end.state = RuleMatchState.Complete;
       yield return end;
     }
