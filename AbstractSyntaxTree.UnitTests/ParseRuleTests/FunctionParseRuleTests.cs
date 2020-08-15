e0238c682838fdccdd68506b2a899cf59cd8d133
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-15 15:14:19 -0400

    Refactor FunctionParseRuleTests

--- AbstractSyntaxTree.UnitTests/ParseRuleTests/FunctionParseRuleTests.cs
+++ AbstractSyntaxTree.UnitTests/ParseRuleTests/FunctionParseRuleTests.cs
@@ -12,30 +12,33 @@ namespace AbstractSyntaxTree.UnitTests
     [Fact]
     public void It_Can_Parse_Empty_Functions()
     {
-      var tokens = new Token[]
-      {
-        Keyword("function"),
-        Word("DoThing"),
-        Symbol("("),
-        Symbol(")"),
-        Symbol("{"),
-        Symbol("}")
-      };
+      NextTokenResult result = ParseFunction("function DoThing(){}");
 
+      Assert.Equal(RuleMatchState.Complete, result.state);
+      var funcDef = Assert.IsAssignableFrom<FunctionDefinition>(result.node);
+      Assert.Equal("DoThing", funcDef.Name);
+      Assert.Empty(funcDef.Statements);
+    }
+
+    private NextTokenResult ParseFunction(string src)
+    {
+      // Get tokens from the src
+      var keywords = new HashSet<string>
+      {
+        "function",
+        "let"
+      };
+      var lexer = new Lexer(keywords);
+      var tokens = lexer.ToTokens(src);
+
+      // Feed all the tokens to the rule
       var rule = new FunctionParseRule();
       NextTokenResult lastResult = default;
 
       foreach (Token t in tokens)
         lastResult = rule.FeedToken(t);
 
-      Assert.Equal(RuleMatchState.Complete, lastResult.state);
-      var funcDef = Assert.IsAssignableFrom<FunctionDefinition>(lastResult.node);
-      Assert.Equal("DoThing", funcDef.Name);
-      Assert.Empty(funcDef.Statements);
+      return lastResult;
     }
-
-    private Token Word(string content) => new Token(DEFAULT_POS, TokenType.Word, content);
-    private Token Keyword(string content) => new Token(DEFAULT_POS, TokenType.Keyword, content);
-    private Token Symbol(string content) => new Token(DEFAULT_POS, TokenType.Symbol, content);
   }
 }
