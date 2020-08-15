b335d7502c487e1f18c1a6aa8eb10484b18bb652
Alex Shelley <alexthepeppershelley@gmail.com>
2020-08-15 14:06:29 -0400

    Add StaticFunctionCallStatemet node, so that functions can have more than one type of rule.

---
+++ AbstractSyntaxTree/Model/StaticFunctionCallStatement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree
+{
+  /// <summary>
+  /// Represents a standalone call to a static function.
+  /// A static function is one that does not belong to an object instance.
+  /// Because this node is a statement(and not an expression), any return
+  /// value from the function is discarded.
+  /// </summary>
+  public class StaticFunctionCallStatement : IStatement
+  {
+    public string FunctionName { get; set; }
+    // TODO: Parameter values
+  }
+}
