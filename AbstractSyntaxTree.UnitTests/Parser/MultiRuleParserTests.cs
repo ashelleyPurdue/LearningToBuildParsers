b185cbe18bae8df74032f3badb8a0ecc816e0ec2
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-05 16:19:10 -0400

    Add MultiRuleParser and its tests

---
+++ AbstractSyntaxTree.UnitTests/Parser/MultiRuleParserTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+using AbstractSyntaxTree.Parser;
+
+namespace AbstractSyntaxTree.UnitTests.Parser
+{
+  public class MultiRuleParserTests
+  {
+    [Fact]
+    public void It_Can_Complete_A_Single_Rule()
+    {
+      bool callbackInvoked = false;
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("A", "simple", "rule"), n => callbackInvoked = true);
+
+      var tokens = WordTokens("A", "simple", "rule");
+      var lastResult = FeedToCoroutine(rules, tokens);
+      lastResult.AssertComplete();
+
+      Assert.True(callbackInvoked);
+    }
+
+    [Fact]
+    public void The_Whole_Thing_Fails_If_All_The_Rules_Fail()
+    {
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("no", "more", "heros"), n => throw new Exception("There were heros."))
+        .AddRule<object>(ExpectsWords("no", "way", "this"), n => throw new Exception("It matched"));
+
+      var tokens = WordTokens("no", "way", "words");
+      var lastResult = FeedToCoroutine(rules, tokens);
+      lastResult.AssertFailed();
+    }
+
+    [Fact]
+    public void The_First_Rule_To_Complete_Wins()
+    {
+      int ruleCompleted = -1;
+
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("not", "even", "close"), n => ruleCompleted = 0)
+        .AddRule<object>(ExpectsWords("no", "more", "time"), n => ruleCompleted = 1)
+        .AddRule<object>(ExpectsWords("no", "more", "time", "left"), n => ruleCompleted = 2);
+
+      var tokens = WordTokens("no", "more", "time");
+      var lastResult = FeedToCoroutine(rules, tokens);
+      lastResult.AssertComplete();
+
+      Assert.Equal(1, ruleCompleted);
+    }
+
+    [Fact]
+    public void The_First_Rule_In_The_List_Wins_If_There_Is_A_Tie()
+    {
+      bool firstCallbackInvoked = false;
+      bool secondCallbackInvoked = false;
+
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("foo", "bar", "baz"), n => firstCallbackInvoked = true)
+        .AddRule<object>(ExpectsWords("foo", "bar", "baz"), n => secondCallbackInvoked = true);
+
+      var tokens = WordTokens("foo", "bar", "baz");
+      var lastResult = FeedToCoroutine(rules, tokens);
+      lastResult.AssertComplete();
+
+      Assert.True(firstCallbackInvoked);
+      Assert.False(secondCallbackInvoked);
+    }
+
+    [Fact]
+    public void It_Throws_An_Exception_If_You_Feed_It_A_Token_After_It_Has_Completed()
+    {
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("foo", "bar", "baz"), n => { });
+
+      var tokens = WordTokens("foo", "bar", "baz");
+      var lastResult = FeedAll(rules, tokens);
+      lastResult.AssertComplete();
+
+      Assert.ThrowsAny<Exception>(() =>
+      {
+        FeedToCoroutine(rules, WordTokens("extra"));
+      });
+    }
+
+    [Fact]
+    public void It_Throws_An_Exception_If_You_Feed_It_A_Token_After_It_Has_Failed()
+    {
+      var rules = new MultiRuleParser()
+        .AddRule<object>(ExpectsWords("foo", "bar", "baz"), n => { });
+
+      var tokens = WordTokens("foo", "bar", "fizz");
+      var lastResult = FeedAll(rules, tokens);
+      lastResult.AssertFailed();
+
+      Assert.ThrowsAny<Exception>(() =>
+      {
+        FeedToCoroutine(rules, WordTokens("extra"));
+      });
+    }
+
+    /// <summary>
+    /// Feeds all tokens to the parser and returns the last result.
+    /// </summary>
+    private RuleResult FeedAll(IRuleParser parser, IEnumerable<Token> tokens)
+    {
+      RuleResult lastResult = default;
+
+      foreach (Token t in tokens)
+        lastResult = parser.FeedToken(t);
+
+      return lastResult;
+    }
+
+    /// <summary>
+    /// Converts the multirule parser into a coroutine.
+    /// Runs the coroutine until it stops, and returns the final result.
+    /// </summary>
+    /// <param name="tokens">The sequence of tokens that the current token callback will return.</param>
+    private RuleResult FeedToCoroutine(MultiRuleParser rules, IEnumerable<Token> tokens)
+    {
+      var tokenMachine = tokens.GetEnumerator();
+      return rules.ToCoroutine(CurrentToken).Last();
+
+      Token CurrentToken()
+      {
+        tokenMachine.MoveNext();
+        return tokenMachine.Current;
+      }
+    }
+
+    private RuleCoroutine ExpectsWords(params string[] words)
+    {
+      return NamingIsHard;
+      IEnumerable<RuleResult> NamingIsHard(CurrentTokenCallback currentToken)
+      {
+        // Expect all the words except the last one
+        for (int i = 0; i < words.Length - 1; i++)
+          yield return Expect.Word(currentToken(), words[i]);
+
+        // The last one needs to include a non-null null value for the
+        // node, so it will be marked as Complete instead of GoodSoFar
+        yield return Expect.Word(currentToken(), words.Last(), true);
+      };
+    }
+
+    private IEnumerable<Token> WordTokens(params string[] words)
+    {
+      int charCount = 0;
+      foreach (string content in words)
+      {
+        var pos = new CodePos(0, charCount);
+        charCount += content.Length + 1; // Add the length of the word, plus an imaginary space.
+
+        yield return new Token(pos, TokenType.Word, content);
+      }
+    }
+  }
+
+}
---
+++ AbstractSyntaxTree/Parser/MultiRuleParser.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractSyntaxTree.Parser
+{
+  public class MultiRuleParser : IRuleParser
+  {
+    private List<IRuleParser> _rules = new List<IRuleParser>();
+
+    private Dictionary<IRuleParser, Action<object>> _ruleCallbacks
+      = new Dictionary<IRuleParser, Action<object>>();
+
+    private bool _isFinished = false;
+
+    public MultiRuleParser AddRule<TNode>(IRuleParser rule, Action<TNode> onCompleted)
+    {
+      _rules.Add(rule);
+      _ruleCallbacks.Add(rule, CallbackWrapper);
+
+      void CallbackWrapper(object uncastedNode)
+      {
+        onCompleted((TNode)uncastedNode);
+      }
+
+      return this;
+    }
+
+    public MultiRuleParser AddRule<TNode>(RuleCoroutine coroutine, Action<TNode> onCompleted)
+    {
+      var rule = new RuleCoroutineParser(coroutine);
+      return AddRule(rule, onCompleted);
+    }
+
+    public RuleResult FeedToken(Token t)
+    {
+      // TODO: Decide what the behavior should be when feeding a token
+      // to an already-finished rule.
+      if (_isFinished)
+        throw new Exception("Tried to feed a token to an already-finished MultiRuleParser.");
+
+      if (_rules.Count == 0)
+        throw new Exception("Tried to feed a token to an unfinished yet empty MultiRuleParser.");
+
+      // Feed the token to each rule
+      var ruleResults = _rules
+        .Select(r => (rule: r, result: r.FeedToken(t)))
+        .ToArray();
+
+      // Eliminate all of the rules that failed
+      foreach (var p in ruleResults)
+      {
+        if (p.result.status == RuleStatus.Failed)
+        {
+          _rules.Remove(p.rule);
+
+          // If that was the last subrule, then the whole
+          // ruleset fails.
+          if (_rules.Count == 0)
+            return p.result;
+        }
+      }
+
+      // If one of the rules succeeds, crown it the winner.
+      // Its result will be returned and its callback will be
+      // invoked.
+      // If more than one rule succeeds, the first rule added
+      // to the list gets priority.
+      var completedRules = ruleResults
+        .Where(p => p.result.status == RuleStatus.Complete);
+
+      if (completedRules.Any())
+      {
+        _isFinished = true;
+
+        var winner = completedRules.First();
+        var node = winner.result.node;
+        
+        _ruleCallbacks[winner.rule](node);
+        return winner.result;
+      }
+
+      // None of them succeeded, but not all of them failed.
+      // That's the definition of being "good so far".
+      return RuleResult.GoodSoFar();
+    }
+  
+    public IEnumerable<RuleResult> ToCoroutine(CurrentTokenCallback t)
+    {
+      RuleResult result;
+      do
+      {
+        result = this.FeedToken(t());
+        yield return result;
+      }
+      while (result.status == RuleStatus.GoodSoFar);
+    }
+  }
+}
