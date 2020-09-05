02a96ef83a219f965aafb1a6bd8750de619b4a6b
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-05 18:25:54 -0400

    Refactor ParseClass to use RepeatedMultiruleParser

---
+++ AbstractSyntaxTree/Parser/RepeatedMultiruleParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree.Parser
+{
+  public class RepeatedMultiruleParser : IRuleParser
+  {
+    public bool IsFinished { get; private set; } = false;
+
+    private Func<Token, bool> _shouldTerminate;
+    private object _node;
+
+    private readonly MultiRuleParser _rules = new MultiRuleParser();
+    private readonly RuleCoroutineParser _impl;
+
+    public RepeatedMultiruleParser()
+    {
+      _impl = new RuleCoroutineParser(ImplCoroutine);
+    }
+
+    public RepeatedMultiruleParser AddRule<TNode>(IRuleParser rule, Action<TNode> onCompleted)
+    {
+      _rules.AddRule<TNode>(rule, onCompleted);
+      return this;
+    }
+
+    public RepeatedMultiruleParser AddRule<TNode>(RuleCoroutine rule, Action<TNode> onCompleted)
+    {
+      _rules.AddRule<TNode>(rule, onCompleted);
+      return this;
+    }
+
+    public RepeatedMultiruleParser TerminatesWhen(Func<Token, bool> shouldTerminate)
+    {
+      _shouldTerminate = shouldTerminate;
+      return this;
+    }
+
+    public RepeatedMultiruleParser YieldsWhenComplete(object node)
+    {
+      _node = node;
+      return this;
+    }
+
+    public RuleResult FeedToken(Token t) => _impl.FeedToken(t);
+
+    public void Reset()
+    {
+      _rules.Reset();
+      _impl.Reset();
+      IsFinished = false;
+    }
+
+    private IEnumerable<RuleResult> ImplCoroutine(CurrentTokenCallback t)
+    {
+      while (!_shouldTerminate(t()))
+      {
+        _rules.Reset();
+
+        // Keep feeding tokens until one of the rules completes, or
+        // until there's an error.
+        RuleResult result = RuleResult.GoodSoFar();
+        while (result.status == RuleStatus.GoodSoFar)
+        {
+          result = _rules.FeedToken(t());
+
+          if (result.status == RuleStatus.GoodSoFar)
+            yield return result;
+        }
+
+        switch (result.status)
+        {
+          case RuleStatus.Complete:
+            yield return RuleResult.GoodSoFar();
+            continue;
+          case RuleStatus.Failed:
+            IsFinished = true;
+            yield return result;
+            yield break;
+          default:
+            IsFinished = true;
+            yield return RuleResult.Failed(t().Position, "Unexpected rule result " + result.status);
+            yield break;
+        }
+      }
+
+      IsFinished = true;
+      yield return RuleResult.Complete(_node);
+    }
+  }
+}
--- AbstractSyntaxTree/Parser/RuleCoroutines/Class.cs
+++ AbstractSyntaxTree/Parser/RuleCoroutines/Class.cs
@@ -15,37 +15,13 @@ namespace AbstractSyntaxTree.Parser
       yield return Extract.Word(t(), name => node.Name = name);
       yield return Expect.Symbol(t(), "{");
 
-      while (!t().IsSymbol("}"))
-      {
-        var classMemberParser = new MultiRuleParser()
-          .AddRule<FunctionDefinition>(ParseFunction, f => node.Functions.Add(f));
+      var classMemberParser = new RepeatedMultiruleParser()
+        .AddRule<FunctionDefinition>(ParseFunction, f => node.Functions.Add(f))
+        .TerminatesWhen(t => t.IsSymbol("}"))
+        .YieldsWhenComplete(node);
 
-        // Otherwise, keep feeding tokens until one of the rules completes, or
-        // until there's an error.
-        RuleResult result = RuleResult.GoodSoFar();
-        while (result.status == RuleStatus.GoodSoFar)
-        {
-          result = classMemberParser.FeedToken(t());
-
-          if (result.status == RuleStatus.GoodSoFar)
-            yield return result;
-        }
-        
-        switch (result.status)
-        {
-          case RuleStatus.Complete:
-            yield return RuleResult.GoodSoFar();
-            continue;
-          case RuleStatus.Failed:
-            yield return result;
-            yield break;
-          default:
-            yield return RuleResult.Failed(t().Position, "Unexpected rule result " + result.status);
-            yield break;
-        }
-      }
-
-      yield return RuleResult.Complete(node);
+      while (!classMemberParser.IsFinished)
+        yield return classMemberParser.FeedToken(t());
     }
   }
 }
