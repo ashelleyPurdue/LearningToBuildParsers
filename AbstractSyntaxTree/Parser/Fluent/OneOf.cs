f2e1c439c243c8c250dafe20fea2da86905aba49
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-06 17:04:55 -0400

    Add OneOf test

---
+++ AbstractSyntaxTree/Parser/Fluent/OneOf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree.Parser.Fluent
+{
+  public class OneOf : IOneOf
+  {
+    public ITheOptions<IOneOfA> The => throw new NotImplementedException();
+
+    public IAOptions<IOneOfA> A => throw new NotImplementedException();
+  }
+}
