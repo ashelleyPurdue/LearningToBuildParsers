7704f7acae538c68208be0f57c98a5c002b19118
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-06 13:32:39 -0400

    Add fluent interfaces and a stub

---
+++ AbstractSyntaxTree/Parser/Fluent/Fluent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractSyntaxTree.Parser.Fluent
+{
+  public static class Starts
+  {
+    public static IWith With() => throw new NotImplementedException();
+  }
+
+  public interface IWith
+  {
+    IWithA A(IRuleParser rule);
+  }
+
+  public interface IWithA
+  {
+    IThen Then();
+    IRuleParser Build();
+  }
+
+  public interface IThen
+  {
+    IThenA A(IRuleParser rule);
+    IOneOf OneOf();
+  }
+
+  public interface IThenA
+  {
+    IThen Then();
+    IRuleParser Build();
+  }
+
+  public interface IOneOf
+  {
+    IOneOfA A(IRuleParser rule);
+  }
+
+  public interface IOneOfA
+  {
+    IOr Or();
+  }
+
+  public interface IOr
+  {
+    IOrA A(IRuleParser rule);
+  }
+
+  public interface IOrA
+  {
+    IThen Then();
+    IOr Or();
+    IRuleParser Build();
+  }
+}
