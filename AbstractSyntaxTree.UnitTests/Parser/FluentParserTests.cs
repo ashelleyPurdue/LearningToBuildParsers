4b0cf662deac3131e4e58a3a13affd57d47140ef
Alex Shelley <alexthepeppershelley@gmail.com>
2020-09-06 15:45:47 -0400

    Add test for composing multiple of them

--- AbstractSyntaxTree.UnitTests/Parser/FluentParserTests.cs
+++ AbstractSyntaxTree.UnitTests/Parser/FluentParserTests.cs
@@ -34,5 +34,47 @@ namespace AbstractSyntaxTree.UnitTests.Parser
       result.AssertComplete();
       Assert.Equal("FooBar", className);
     }
+
+    [Fact]
+    public void You_Can_Compose_Multiple_Rules()
+    {
+      string funcName = null;
+      string className = null;
+
+      var funcParser = Starts.With().The.Keyword("function")
+        .Then().A.Word(n => funcName = n)
+        .Then().The.Symbol("{")
+        .Then().The.Symbol("}")
+        .Build();
+
+      var classParser = Starts.With().The.Keyword("class")
+        .Then().A.Word(n => className = n)
+        .Then().The.Symbol("(")
+        .Then().The.Symbol(")")
+        .Then().The.Symbol("{")
+        .Then().A.Rule<object>(funcParser, node => { }) // TODO: test that it returns some kind of function object
+        .Then().The.Symbol("}")
+        .Build();
+
+      // Generate some tokens to parse with the parser we just made
+      string src = 
+      @"
+        class FooBar
+        {
+          function DoThing() {}
+        }
+      ";
+      var tokens = new Lexer().ToTokens(src);
+
+      // Parse them
+      RuleResult result = RuleResult.GoodSoFar();
+      foreach (var token in tokens)
+        result = classParser.FeedToken(token);
+
+      // Assert that it correctly did stuff.
+      result.AssertComplete();
+      Assert.Equal("FooBar", className);
+      Assert.Equal("DoThing", funcName);
+    }
   }
 }
